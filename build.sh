c40fbc2e1c92743c7aa16945092eb0235c67ece5
Egor Kalinin <egorsinbox@gmail.com>
2024-08-31 16:09:20 +0300

    wip

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-PROJECT_DIR=$(cd "$(dirname $(readlink -f "${BASH_SOURCE[0]}"))" && pwd)
+project_dir=$(cd "$(dirname $(readlink -f "${BASH_SOURCE[0]}"))" && pwd)
 
-md_path_prefix=$PROJECT_DIR/md
+md_path_prefix=$project_dir/md
+html_path_prefix=$project_dir/docs
+mkdir -p $html_path_prefix
 
 for md_path in $(find $md_path_prefix -name "*.md")
 do
     md_path_suffix=${md_path#$md_path_prefix}
-    html_path=$PROJECT_DIR/html${md_path_suffix%.md}.html
+    html_path=$html_path_prefix${md_path_suffix%.md}.html
     echo "$md_path -> $html_path"
     pandoc -s -f markdown -t html5 --shift-heading-level-by=1 -o $html_path $md_path
 done
